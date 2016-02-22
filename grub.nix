2f3d10b5823ca1a8cf50d46fa965afb27cdb66d9
Arian van Putten <aeroboy94@gmail.com>
2016-02-22 16:13:59 +0100

    remove excess

--- grub.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, ...}:
-{
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/vda";
-}
--- services.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ...}:
-{
-  services.openssh = {
-    enable = true;
-    passwordAuthentication = false;
-  };
-}
