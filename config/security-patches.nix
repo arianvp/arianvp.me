d3073aeb86f2861022cb540aa7155fe293296023
Arian van Putten <aeroboy94@gmail.com>
2016-09-29 11:42:02 +0200

    Remove security patch

--- config/security-patches.nix
+++ config/security-patches.nix
@@ -2,9 +2,10 @@
 {
   system.replaceRuntimeDependencies = with pkgs.lib;
       # cve-20215-7547
-      [{original = pkgs.glibc; replacement = pkgs.stdenv.lib.overrideDerivation pkgs.glibc (oldAttr: { patches = oldAttr.patches ++
-        [(pkgs.fetchurl { url = "https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/libraries/glibc/cve-2015-7547.patch";
-                          sha256 = "0awpc4rp2x27rjpj83ps0rclmn73hsgfv2xxk18k82w4hdxqpp5r";})];
-       });}
+      [
+        # {original = pkgs.glibc; replacement = pkgs.stdenv.lib.overrideDerivation pkgs.glibc (oldAttr: { patches = oldAttr.patches ++
+        # [(pkgs.fetchurl { url = "https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/libraries/glibc/cve-2015-7547.patch";
+        #                  sha256 = "0awpc4rp2x27rjpj83ps0rclmn73hsgfv2xxk18k82w4hdxqpp5r";})];
+        # });}
       ];
 }
